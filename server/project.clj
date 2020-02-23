12fd5c7daa3b4aec6a432646c8704d04539e95a8
Ben Oliver <benjamin.e.oliver@gmail.com>
2020-02-23 22:12:57 +0100

    remove logger missing errors

--- server/project.clj
+++ server/project.clj
@@ -5,7 +5,8 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [cheshire "5.10.0"]
-                 [org.eclipse.jgit/org.eclipse.jgit "5.6.1.202002131546-r"]]
+                 [org.eclipse.jgit/org.eclipse.jgit "5.6.1.202002131546-r"]
+                 [org.slf4j/slf4j-simple "1.7.30"]]
   :main ^:skip-aot language-server-playground.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
